b667c0e832296efda84bc78e151522b405a636bd
wajo10 <43227625+wajo10@users.noreply.github.com>
2021-03-10 17:21:03 -0600

    Update worker-form.component.ts

--- Jest/src/app/workers/new-worker/worker-form/worker-form.component.ts
+++ Jest/src/app/workers/new-worker/worker-form/worker-form.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {WorkerModel} from '../../worker.model';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-
+// Código adaptado desde https://www.youtube.com/watch?v=PdVerlfmO6M&list=PLtlqfp1XEW4hXv8C2-gJVdLTzTVzihoH8&index=1&ab_channel=TheRyanSmee
 @Component({
   selector: 'app-worker-form',
   templateUrl: './worker-form.component.html',
