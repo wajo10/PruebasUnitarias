2141f0ebca45263b1e156755516bbdf1f6e4020d
wajo10 <43227625+wajo10@users.noreply.github.com>
2021-03-10 18:23:00 -0600

    Update worker-form.component.spec.ts

--- Jest/src/app/workers/new-worker/worker-form/worker-form.component.spec.ts
+++ Jest/src/app/workers/new-worker/worker-form/worker-form.component.spec.ts
@@ -1,6 +1,8 @@
 import { WorkerFormComponent } from './worker-form.component';
 import {WorkerModel} from '../../worker.model';
+
 // Código adaptado desde https://www.youtube.com/watch?v=PdVerlfmO6M&list=PLtlqfp1XEW4hXv8C2-gJVdLTzTVzihoH8&index=1&ab_channel=TheRyanSmee
+
 describe( 'WorkerFormComponent', () => { // Probar el componente
   let fixture: WorkerFormComponent; // Clase a probar
   // Son mock porque no interesa realmente el valor
@@ -24,12 +26,12 @@ describe( 'WorkerFormComponent', () => { // Probar el componente
     );
   });
 
-  describe('Setup component', () => {
+  describe('Setup component', () => { // Probar el método Setup Component
     describe('ngOnInit', () => {
-      it('should call generateWorkerForm with this.worker', () => {
-        const generateWorkerFormSpy = jest.spyOn(fixture, 'generateWorkerForm');
+      it('Llamará generateWorkerForm con el parámetro this.worker', () => {
+        const generateWorkerFormSpy = jest.spyOn(fixture, 'generateWorkerForm'); // Spy para analizar luego
         const worker: WorkerModel = {
-          id: 'Tranajador 1',
+          id: 'Trabajador 1',
           name: 'Wajib Zaglul'
         } as WorkerModel;
         fixture.worker = worker;
@@ -37,6 +39,7 @@ describe( 'WorkerFormComponent', () => { // Probar el componente
         fixture.ngOnInit();
 
         expect(generateWorkerFormSpy).toBeCalledWith(worker);
+        // Se espera que generateWorker tenga como parámetro worker de forma correcta
       });
     });
   });
